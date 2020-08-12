97b5ededd88d674075e89d05baf2e92cd62d2088
teknocips78 <69575369+teknocips78@users.noreply.github.com>
2020-08-12 15:11:51 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.enafecode(a_to_vi);
-a_to_ve[i].href = "http://safe.teknocips.com/safelink.html?url=" + encryptedUrl;
+a_to_ve[i].href = "http://safe.teknocips.com/safelink/?url=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
